23519677364f766414e50c6a3835fdbacbddb4d7
Apurv Anand <apurv.nnd@gmail.com>
2017-08-14 09:35:51 +0530

    Update wine.py

--- wine.py
+++ wine.py
@@ -11,6 +11,14 @@ dataset = pd.read_csv('wine.data')
 X = dataset.iloc[:, 1:14].values
 y = dataset.iloc[:, :1].values
 
+#Error mention: hadn't applied onehotencoder before
+#onehotencoder applied on y dataset 
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder = OneHotEncoder(categorical_features = [0])
+y = onehotencoder.fit_transform(y).toarray()
+#y = y[:, 1:] #to prevent data duplicy 
+#Note: no need for above mentioned code for data duplicy
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -39,13 +47,16 @@ classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu', inpu
 classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu'))
 
 #Adding the output layer
-classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(output_dim = 3, init = 'uniform', activation = 'softmax')) 
+#edit: output_dim changed to 3 and activation changed from sigmoid to softmax because of multiple outputs
 
 #Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+#edit: loss changed from binary to categorical_crossentropy 
 
 #Fitting the ANN to the training set
 classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 50)
+
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
